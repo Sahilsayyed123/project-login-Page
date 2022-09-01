61ae2082914073b1250fb9afe45d3321d1f1436f
Sahilsayyed123 <sahilsayyed0409@gmail.com>
2022-09-01 17:18:46 +0530

    color

--- logged_in.py
+++ logged_in.py
@@ -4,7 +4,7 @@ from tkinter import *
 ws = Tk()
 ws.geometry('400x300')
 ws.title('Welcome!')
-ws['bg']='violet'
+
 
 f = ("Times New Roman", 14)
 
@@ -17,8 +17,9 @@ Label(
     text='''YOU HAVE SUCCESSFULLY LOGGED IN''',
     padx=20,
     pady=20,
-    bg='violet',
-    font=f
+    bg='#80558C',
+    font=f,
+    fg="white"
 ).pack(expand=True, fill=BOTH)
 
 
